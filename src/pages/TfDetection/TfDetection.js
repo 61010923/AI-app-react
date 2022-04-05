b845108f733f8c71d4ff23493dfcdfa91b3c3a1b
vanakorn <61010923@kmitl.ac.th>
2022-04-05 16:04:35 +0700

    mask detect

--- src/pages/TfDetection/TfDetection.js
+++ src/pages/TfDetection/TfDetection.js
@@ -2,18 +2,30 @@
 import React, { useRef, useState, useEffect } from 'react'
 import * as tf from '@tensorflow/tfjs'
 import Webcam from 'react-webcam'
-import './App.css'
 import { nextFrame } from '@tensorflow/tfjs'
 // 2. TODO - Import drawing utility here
-import { Box } from '@mui/material'
+import {
+  Box, Button, Switch, FormControlLabel,
+} from '@mui/material'
+import Lottie from 'react-lottie'
 import { drawRect } from './utilities'
+import animationData from '../../lotties/infinity.json'
 
 function App() {
   const webcamRef = useRef(null)
   const canvasRef = useRef(null)
-
+  const [running, setRunning] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const handleUserMedia = () => setTimeout(() => setLoading(false), 1_000)
   // Main function
-
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  }
   const detect = async (net) => {
     // Check data is available
     if (
@@ -53,7 +65,7 @@ function App() {
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)
-      requestAnimationFrame(() => { drawRect(boxes[0], classes[0], scores[0], 0.8, videoWidth, videoHeight, ctx) })
+      requestAnimationFrame(() => { drawRect(boxes[0], classes[0], scores[0], 0.9, videoWidth, videoHeight, ctx) })
 
       tf.dispose(img)
       tf.dispose(resized)
@@ -62,52 +74,95 @@ function App() {
       tf.dispose(obj)
     }
   }
-
+  // eslint-disable-next-line consistent-return
   useEffect(() => {
-    const runCoco = async () => {
-      // 3. TODO - Load network
-      // const net = await tf.loadGraphModel('https://modeltf.s3.us-west-2.amazonaws.com/model.json')
-      const net = await tf.loadGraphModel('https://maskmodel.s3.us-west-2.amazonaws.com/model.json')
-      // Loop and detect hands
-      setInterval(() => {
-        detect(net)
-      }, 16.7)
+    if (running) {
+      let refreshIntervalId
+      const runCoco = async () => {
+        console.log('runcoco')
+        // 3. TODO - Load network
+        // const net = await tf.loadGraphModel('https://modeltf.s3.us-west-2.amazonaws.com/model.json')
+        const net = await tf.loadGraphModel('https://maskmodel.s3.us-west-2.amazonaws.com/model.json')
+        // Loop and detect hands
+        refreshIntervalId = setInterval(() => {
+          detect(net)
+        }, 16.7)
+      }
+      runCoco()
+      return () => clearInterval(refreshIntervalId)
     }
-    runCoco()
-  }, [])
+  }, [running])
 
   return (
-  // <div className="App">
-    <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-      <Box sx={{ display: 'relative' }}>
-        <Webcam
-          ref={webcamRef}
-          muted
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            zIndex: 9,
-            width: 640,
-            height: 480,
-          }}
-        />
+    <>
+      <Button
+        color={running ? 'error' : 'primary'}
+        onClick={() => setRunning(!running)}
+      >
+        {running ? 'stop' : 'start'}
+      </Button>
 
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: 'absolute',
-            left: 0,
-            right: 0,
-            zIndex: 10,
-            width: 640,
-            height: 480,
-          // backgroundColor: 'red',
-          }}
-        />
+      {/* {switchDetect ? (
+        <Button variant="contained" onClick={() => runCoco('start')}>start</Button>
+
+      )
+        : (
+          <Button variant="contained" onClick={() => runCoco('stop')}>stop</Button>
+
+        )} */}
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Box sx={{ display: 'relative' }}>
+          {loading
+            && (
+              <Box
+                style={{
+                  position: 'absolute',
+                  left: 0,
+                  right: 0,
+                  zIndex: 9,
+                  width: 640,
+                  height: 480,
+                }}
+              >
+                <Lottie
+                  options={defaultOptions}
+                  height={400}
+                  width={400}
+                />
+              </Box>
+            )}
+
+          <Webcam
+            ref={webcamRef}
+            muted
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              zIndex: 9,
+              width: 640,
+              height: 480,
+            }}
+            onUserMedia={handleUserMedia}
+          />
+
+          <canvas
+            ref={canvasRef}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              zIndex: 10,
+              width: 640,
+              height: 480,
+              display: (running ? 'block' : 'none'),
+              // backgroundColor: 'red',
+            }}
+          />
+        </Box>
       </Box>
-    </Box>
-  // </div>
+
+    </>
   )
 }
 
