df59c54f947fc6bbf0796abcf72167d3ce104b35
atipattam <tamkjubu@gmail.com>
2022-05-22 15:07:23 +0700

    rm path

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import {
   Route,
 } from 'react-router-dom'
 import React from 'react'
-import ObjDetection from './pages/ObjDetection'
-import TfDetection from './pages/TfDetection'
+
 import PageUI from './pages/PageUI'
 
 function App() {
   return (
 
     <Routes>
-      <Route path="/ObjDetection" element={<ObjDetection />} />
-      <Route path="/TfDetection" element={<TfDetection />} />
+
       <Route path="/" element={<PageUI />} />
     </Routes>
 
